d565d0c36da5a5ee01ae6ecbc3ba5fa24d9f2b1c
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-03-27 02:29:00 -0400

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>Space Guys</Head>
+      <main>space guys</main>
+    </>
+  );
+};
+
+export default Home;
